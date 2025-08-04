c65d1ea7eb13bccbff8082d71f39d0439508fc71
Harsh <sinhaharsh222@gmail.com>
2025-08-04 16:14:19 +0000

    make it more react based and more techy type it looking too proffesional

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,9 +17,9 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"PT Sans"', 'sans-serif'],
-        code: ['monospace'],
+        body: ['"Inter"', 'sans-serif'],
+        headline: ['"Fira Code"', 'monospace'],
+        code: ['"Fira Code"', 'monospace'],
       },
       colors: {
         background: 'hsl(var(--background))',
